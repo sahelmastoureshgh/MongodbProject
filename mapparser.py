ece9072c4eff9f401b88a919d66800d3abf7caa0
sahel mastoureshgh <sahel.mastoureshgh@gmail.com>
2014-04-25 20:34:29 -0400

    read and audit open street map for part of boston
    read and audit open street map for part of boston

---
+++ mapparser.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Your task is to use the iterative parsing to process the map file and
+find out not only what tags are there, but also how many, to get the
+feeling on how much of which data you can expect to have in the map.
+The output should be a dictionary with the tag name as the key
+and number of times this tag can be encountered in the map as value.
+
+Note that your code will be tested with a different data file than the 'example.osm'
+"""
+import xml.etree.ElementTree as ET
+import pprint
+
+def count_tags(filename):
+        # YOUR CODE HERE
+        tags={}
+        osm_file = open(filename, "r")
+        for event,elem in ET.iterparse(osm_file):
+            if elem.tag in tags:
+                tags[elem.tag]=tags[elem.tag]+1
+            else:
+                tags[elem.tag]=1
+            elem.clear()    
+        return tags        
+
+
+
+def test():
+
+    tags = count_tags('example.osm')
+    pprint.pprint(tags)
+    assert tags == {'bounds': 1,
+                     'member': 3,
+                     'nd': 4,
+                     'node': 20,
+                     'osm': 1,
+                     'relation': 1,
+                     'tag': 7,
+                     'way': 1}
+
+    
+
+if __name__ == "__main__":
+    test()
